951b065a5f611059e4c8c14462f7f75e67cf4b02
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-27 11:18:06 +0100

    Added city prefab and script

---
+++ GGJ2018_Project/Assets/Scripts/City.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour, iClickObject
+{
+	public System.Action<City> OnCityClicked = delegate {};
+
+	public void OnClicked()
+	{
+		OnCityClicked(this);
+	}
+}
