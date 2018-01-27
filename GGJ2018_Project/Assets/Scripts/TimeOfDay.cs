2bade0c226b8842fdb7fa05a4423184077b4f7f9
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-27 22:37:14 +0100

    Added timeline callbacks

--- GGJ2018_Project/Assets/Scripts/TimeOfDay.cs
+++ GGJ2018_Project/Assets/Scripts/TimeOfDay.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+
+public struct TimeMarker
+{
+	public float Time;
+	public System.Action<object> Callback;
+	public object Argument;
+}
 
 public class TimeOfDay : MonoBehaviour
 {
@@ -17,18 +24,48 @@ public class TimeOfDay : MonoBehaviour
 
 	public System.Action OnDayPassed = delegate { };
 
+	private readonly List<TimeMarker> m_TimelineMarkers = new List<TimeMarker>();
+
 	public virtual void Update()
 	{
 		if (!m_TimerPaused)
 		{
 			m_CurrentTime = Mathf.Clamp01(m_CurrentTime + Time.deltaTime / m_DayDuration);
+
 			m_TextMesh.text = GetTimeOfDayFormatted();
+			UpdateTimelineMarkers(GetTimeOfDay());
+
 			if (m_CurrentTime == 1)
 			{
+				OnDayPassed();
 				m_TextMesh.text = "Day over";
 				m_TimerPaused = true;
 			}
 
+		}
+	}
+
+	private void UpdateTimelineMarkers(float a_NewTime)
+	{
+		int markerCalls = 0;
+
+		foreach (var marker in m_TimelineMarkers)
+		{
+			if (marker.Time < a_NewTime)
+			{
+				marker.Callback(marker.Argument);
+				++markerCalls;
+			}
+			else
+			{
+				//List is sorted
+				break;
+			}
+		}
+
+		if (markerCalls > 0)
+		{
+			m_TimelineMarkers.RemoveRange(0, markerCalls);
 		}
 	}
 
@@ -49,6 +86,17 @@ public class TimeOfDay : MonoBehaviour
 		float time = GetTimeOfDay();
 		int hours = (int) time;
 		int minutes = (int)((time - hours) * 60);
-		return String.Format("{0}{1}:{2}{3}", hours < 10 ? "0" : "", hours, minutes < 10 ? "0" : "", minutes);
+		return string.Format("{0}{1}:{2}{3}", hours < 10 ? "0" : "", hours, minutes < 10 ? "0" : "", minutes);
+	}
+
+	public void AddTimelineMarker(float a_Time, System.Action<object> a_Callback, object a_Argument = null)
+	{
+		TimeMarker marker;
+		marker.Time = a_Time;
+		marker.Callback = a_Callback;
+		marker.Argument = a_Argument;
+
+		m_TimelineMarkers.Add(marker);
+		m_TimelineMarkers.Sort((a, b) => a.Time.CompareTo(b.Time));
 	}
 }
