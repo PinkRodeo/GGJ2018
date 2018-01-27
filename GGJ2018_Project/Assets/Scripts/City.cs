11b91301bb013e0cda9d10a46ca35067d964a1dd
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-27 21:34:57 +0100

    Added city name on hover

--- GGJ2018_Project/Assets/Scripts/City.cs
+++ GGJ2018_Project/Assets/Scripts/City.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class City : MonoBehaviour
 {
+	[SerializeField]
+	private TextMeshPro m_TextMesh;
+
 	public System.Action<City> OnCityClicked = delegate {};
 
 	private void OnMouseDown()
 	{
 		OnCityClicked(this);
 	}
+
+	private void OnMouseEnter()
+	{
+		m_TextMesh.gameObject.SetActive(true);
+	}
+
+	private void OnMouseExit()
+	{
+		m_TextMesh.gameObject.SetActive(false);
+	}
+
 }
