a8daada959bbdda4054a5179a62f1168f9bc1a15
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-27 19:12:14 +0100

    old stuff apparently

--- GGJ2018_Project/Assets/WundeeScripts/Unity/GameEntry.cs
+++ GGJ2018_Project/Assets/WundeeScripts/Unity/GameEntry.cs
@@ -25,31 +25,5 @@ namespace WundeeUnity
 		{
 			game.Update(UnityEngine.Time.deltaTime);
 		}
-
-		protected void FixedUpdate()
-		{
-			if (Input.GetKey(KeyCode.LeftBracket))
-			{
-				Wundee.Time.multiplier -= 0.5d;
-				Debug.Log(Wundee.Time.multiplier);
-			}
-
-			if (Input.GetKey(KeyCode.RightBracket))
-			{
-				Wundee.Time.multiplier += 0.5d;
-				Debug.Log(Wundee.Time.multiplier);
-			}
-
-			game.FixedUpdate(UnityEngine.Time.fixedDeltaTime);
-
-			if (Input.GetKeyUp(KeyCode.R))
-			{
-				Game.instance.reloadGame = true;
-				return;
-			}
-
-
-		}
 	}
-
 }
