3a2a8bd8bd3ededf3684e50ed1abd20069d791c7
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-28 11:54:12 +0100

    Added event binding to kingdom game entry

--- GGJ2018_Project/Assets/Kingdom/KingdomGameEntry.cs
+++ GGJ2018_Project/Assets/Kingdom/KingdomGameEntry.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,8 +10,22 @@ namespace Kingdom
 {
     public class KingdomGameEntry : WundeeUnity.GameEntry
     {
-        // Use this for initialization
-	    public void Start()
+		[SerializeField]
+	    private struct TimelineEvent
+	    {
+		    public string Name;
+		    public float Time;
+		}
+
+		[SerializeField]
+	    private TimeOfDay m_GameTimer;
+		[SerializeField]
+	    private TimelineEvent[] m_Events;
+
+	    private System.Action<object> m_TimelineCallback;
+
+		// Use this for initialization
+		public void Start()
 	    {
 		    /*
 		    var location = new Location(game.definitions.locationDefinitions["PERSON_DEFAULT_01"]);
@@ -33,6 +48,39 @@ namespace Kingdom
 		    {
 			    SceneManager.LoadScene("ConversationUI", LoadSceneMode.Additive);
 		    }
+
+			
+		    
+		    Debug.Log(Game.instance.conversationUI);
+
+		    m_TimelineCallback += ExecuteEffect;
+			m_GameTimer.OnDayPassed += OnDayCompleted; 
+		    Game.instance.definitions.effectDefinitions[""].GetConcreteType(); //TODO: need some kind of callback to unpause game
+
+		    foreach (var timelineEvent in m_Events)
+		    {
+			    m_GameTimer.AddTimelineMarker(timelineEvent.Time, m_TimelineCallback, timelineEvent.Name);
+			}
+
+		    StartCoroutine(DelayedStart);
 	    }
-    }
+
+	    private void ExecuteEffect(object Name)
+	    {
+		    Game.instance.definitions.effectDefinitions[(string)Name].GetConcreteType().ExecuteEffect();
+		    m_GameTimer.SetPaused(true);
+	    }
+
+	    private void OnDayCompleted()
+	    {
+			//TODO what to do when day ends
+		    m_GameTimer.RestartDayTimer();
+		}
+
+	    private IEnumerator DelayedStart()
+	    {
+		    yield return new WaitForEndOfFrame();
+
+	    }
+	}
 }
