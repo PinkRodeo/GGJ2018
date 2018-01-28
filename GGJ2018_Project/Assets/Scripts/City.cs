d0474cbdd767910405b8f78c4885e74f91716ee0
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-28 01:30:28 +0100

    Added exclamation mark

--- GGJ2018_Project/Assets/Scripts/City.cs
+++ GGJ2018_Project/Assets/Scripts/City.cs
@@ -20,9 +20,18 @@ public class City : MonoBehaviour
     private Location m_Location;
     [SerializeField]
 	private TextMeshPro m_TextMesh;
+	[SerializeField]
+	private SpriteMouseEvents m_EventMarker;
 
 
 	public System.Action<City> OnCityClicked = delegate {};
+
+	public virtual void Start()
+	{
+		m_EventMarker.MouseDown += OnMouseDown;
+		m_EventMarker.MouseEnter += OnMouseEnter;
+		m_EventMarker.MouseExit += OnMouseExit;
+	}
 
 	private void OnMouseDown()
 	{
@@ -45,4 +54,8 @@ public class City : MonoBehaviour
         m_TextMesh.text = location.definition.name;
     }
 
+	public void SetEventActive(bool a_Active)
+	{
+		m_EventMarker.gameObject.SetActive(a_Active);
+	}
 }
---
+++ GGJ2018_Project/Assets/Scripts/SpriteMouseEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpriteMouseEvents : MonoBehaviour
+{
+	public System.Action MouseEnter;
+	public System.Action MouseExit;
+	public System.Action MouseDown;
+
+	private void OnMouseDown()
+	{
+		MouseDown();
+	}
+
+	private void OnMouseExit()
+	{
+		MouseExit();
+	}
+
+	private void OnMouseEnter()
+	{
+		MouseEnter();
+	}
+}
