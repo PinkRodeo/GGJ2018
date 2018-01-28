9511abee3265b8f9201b0c3061edf10a62c8226b
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-28 04:20:16 +0100

    HUD now is bound to data

--- GGJ2018_Project/Assets/Scripts/StatusHUD.cs
+++ GGJ2018_Project/Assets/Scripts/StatusHUD.cs
@@ -1,34 +1,61 @@
 ﻿using System;
 using TMPro;
 using UnityEngine;
+using Wundee;
 
 public class StatusHUD : MonoBehaviour
 {
 	[SerializeField]
-	private enum DigitType
+	private enum HUDType
 	{
-		Integer,
-		Percentage
+		Days,
+		Gold,
+		Pawns,
+		Reputation
 	}
 
 	[SerializeField]
-	private DigitType m_DigitType;
+	private HUDType m_HUDType;
+	[SerializeField]
+	private string m_Faction;
 	[SerializeField]
 	private TextMeshPro m_DigitMesh;
 
 	private float m_Value;
-	
+
+	public virtual void Start()
+	{
+		switch (m_HUDType)
+		{
+			case HUDType.Days:
+				break;
+			case HUDType.Gold:
+				Game.instance.world.OnGoldChanged += SetValue;
+				SetValue(0, Game.instance.world.gold);
+				break;
+			case HUDType.Pawns:
+				Game.instance.world.OnPawnsChanged += SetValue;
+				SetValue(0, Game.instance.world.pawns);
+				break;
+			case HUDType.Reputation:
+				Game.instance.world.factions[m_Faction].OnReputationChanged += SetValue;
+				SetValue(0, Game.instance.world.factions[m_Faction].reputation);
+				break;
+		}
+	}
 
 	private void UpdateText()
 	{
-		switch (m_DigitType)
+		switch (m_HUDType)
 		{
-			case DigitType.Integer:
+			case HUDType.Days:
+			case HUDType.Gold:
+			case HUDType.Pawns:
 			{
 				m_DigitMesh.text = Mathf.RoundToInt(m_Value) + "";
 				break;
 			}
-			case DigitType.Percentage:
+			case HUDType.Reputation:
 			{
 				m_DigitMesh.text = m_Value + "%";
 				break;
@@ -36,8 +63,14 @@ public class StatusHUD : MonoBehaviour
 		}
 	}
 
-	public void SetValue(float a_Value)
+	public void SetValue(int a_Old, int a_New)
 	{
-		m_Value = a_Value;
+		m_Value = a_New;
+		UpdateText();
+	}
+
+	public void SetValue(int a_Old, int a_New, Wundee.Faction a_Faction)
+	{
+		SetValue(a_Old, a_New);
 	}
 }
