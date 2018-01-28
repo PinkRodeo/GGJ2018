36cf235e08bda0f363092fbfc0c63fd3f7e2bc77
Weikie Yeh <weikie_yeh@hotmail.com>
2018-01-28 12:34:43 +0100

    unpause game when pressing event button

--- GGJ2018_Project/Assets/Kingdom/KingdomGameEntry.cs
+++ GGJ2018_Project/Assets/Kingdom/KingdomGameEntry.cs
@@ -8,25 +8,26 @@ using Wundee.Stories;
 
 namespace Kingdom
 {
-    public class KingdomGameEntry : WundeeUnity.GameEntry
-    {
+	public class KingdomGameEntry : WundeeUnity.GameEntry
+	{
 		[System.Serializable]
-	    private struct TimelineEvent
-	    {
-		    public string RewardName;
+		private struct TimelineEvent
+		{
+			public string RewardName;
 			public float Time;
 		}
 
 
 		[SerializeField]
-	    private List<TimelineEvent> m_Events = new List<TimelineEvent>();
+		private List<TimelineEvent> m_Events = new List<TimelineEvent>();
 
-	    private System.Action<object> m_TimelineCallback;
+		private System.Action<object> m_TimelineCallback;
+		private ConversationUI m_ConversationUI;
+		private List<ConversationButtonUI> m_Buttons;
 
-		// Use this for initialization
 		public void Start()
-	    {
-		    /*
+		{
+			/*
 		    var location = new Location(game.definitions.locationDefinitions["PERSON_DEFAULT_01"]);
 
 		    var spyMessageDefinition = game.definitions.storyNodeDefinitions["SPY_TEST_1"];
@@ -41,45 +42,71 @@ namespace Kingdom
 		    }
 		    */
 
-		    Scene scene = SceneManager.GetSceneByName("ConversationUI");
+			Scene scene = SceneManager.GetSceneByName("ConversationUI");
 
 			if (!scene.isLoaded)
-		    {
-			    SceneManager.LoadScene("ConversationUI", LoadSceneMode.Additive);
-		    }
-
-			
-		    
-
-		    m_TimelineCallback += ExecuteEffect;
-			m_GameTimer.OnDayPassed += OnDayCompleted; 
-		    //Game.instance.definitions.effectDefinitions[""].GetConcreteType(); //TODO: need some kind of callback to unpause game
-
-		    foreach (var timelineEvent in m_Events)
-		    {
-			    m_GameTimer.AddTimelineMarker(timelineEvent.Time, m_TimelineCallback, timelineEvent.RewardName);
+			{
+				SceneManager.LoadScene("ConversationUI", LoadSceneMode.Additive);
 			}
-	
-		    StartCoroutine("DelayedStart");
-	    }
 
-	    private void ExecuteEffect(object Name)
-	    {
-		    Game.instance.definitions.effectDefinitions[(string)Name].GetConcreteType().ExecuteEffect();
-		    m_GameTimer.SetPaused(true);
-	    }
 
-	    private void OnDayCompleted()
-	    {
-			//TODO what to do when day ends
-		    m_GameTimer.RestartDayTimer();
+
+
+			m_TimelineCallback += ExecuteEffect;
+			m_GameTimer.OnDayPassed += OnDayCompleted;
+			//Game.instance.definitions.effectDefinitions[""].GetConcreteType(); //TODO: need some kind of callback to unpause game
+
+			foreach (var timelineEvent in m_Events)
+			{
+				m_GameTimer.AddTimelineMarker(timelineEvent.Time, m_TimelineCallback, timelineEvent.RewardName);
+			}
+
+			StartCoroutine("DelayedStart");
 		}
 
-	    private IEnumerator DelayedStart()
-	    {
-		    yield return new WaitForEndOfFrame();
-		    //Debug.Log(Game.instance.conversationUI);
+		private void ExecuteEffect(object Name)
+		{
+			Game.instance.definitions.effectDefinitions[(string)Name].GetConcreteType().ExecuteEffect();
+			m_GameTimer.SetPaused(true);
+			BindButtonEvents();
+		}
 
+		private void BindButtonEvents()
+		{
+			m_Buttons = Game.instance.conversationUI.GetChoiceButtons();
+			m_Buttons.ForEach(button => button.Button.onClick.AddListener(ChoiceButtonClicked));
+		}
+
+		private void OnDayCompleted()
+		{
+			//TODO what to do when day ends
+			m_GameTimer.RestartDayTimer();
+		}
+
+		private IEnumerator DelayedStart()
+		{
+			yield return new WaitForEndOfFrame();
+			m_ConversationUI = Game.instance.conversationUI;
+		}
+
+		private void ChoiceButtonClicked()
+		{
+			StartCoroutine("DelayedButtonClick");
+		}
+
+		private IEnumerator DelayedButtonClick()
+		{
+			yield return new WaitForSeconds(0.5f);
+
+			if (Game.instance.conversationUI.GetChoiceButtons().Count == 0)
+			{
+				m_Buttons.ForEach(button => button.Button.onClick.RemoveListener(ChoiceButtonClicked));
+				m_GameTimer.SetPaused(false);
+			}
+			else
+			{
+				BindButtonEvents();
+			}
 		}
 	}
 }
